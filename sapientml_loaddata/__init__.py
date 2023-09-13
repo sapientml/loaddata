aa2bf0468de575e29ed2c1fec0f93b488c6f67bf
Akira Ura <ura.akira@jp.fujitsu.com>
2023-09-13 20:10:34 +0900

    feat: handle ignored id columns
    Signed-off-by: Akira Ura <ura.akira@jp.fujitsu.com>

--- sapientml_loaddata/__init__.py
+++ sapientml_loaddata/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .generator import LoadData
+from .generator import LoadData, LoadDataConfig
 
-__all__ = ["LoadData"]
+__all__ = ["LoadData", "LoadDataConfig"]
--- sapientml_loaddata/generator.py
+++ sapientml_loaddata/generator.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 
 import os
+from typing import Optional
 
 from jinja2 import Environment, FileSystemLoader
 from sapientml.generator import CodeBlockGenerator
-from sapientml.params import Code, Dataset, Task
+from sapientml.params import Code, Config, Dataset, Task
 
 template_env = Environment(loader=FileSystemLoader(f"{os.path.dirname(__file__)}/templates"), trim_blocks=True)
 ROW_THRESHOLD_FOR_SAMPLING = 100000
@@ -27,7 +28,14 @@ def _render(tpl, *args, **kwargs):
     return "\n".join([line for line in code.split("\n") if len(line) > 0]) + "\n\n"
 
 
+class LoadDataConfig(Config):
+    id_columns_for_prediction: Optional[list[str]] = None
+
+
 class LoadData(CodeBlockGenerator):
+    def __init__(self, **kwargs):
+        self.config = LoadDataConfig(**kwargs)
+
     def generate_code(self, dataset: Dataset, task: Task):
         """Generates validation, test, train and predict code snippets for the pipeline.
 
@@ -46,30 +54,90 @@ class LoadData(CodeBlockGenerator):
         dataset, code : Tuple[Dataset, Code]
 
         """
+
         code = Code()
         code.validation = code.test = code.train = code.predict = "# *** GENERATED PIPELINE ***\n\n"
 
-        tpl = template_env.get_template("loaddata.py.jinja")
-        code.validation += _render(tpl, dataset=dataset, task=task, validation=True)
-        code.test += _render(tpl, dataset=dataset, task=task, validation=False)
-        tpl = template_env.get_template("loaddata_train.py.jinja")
-        code.train += _render(tpl, dataset=dataset, task=task, script_type="train")
-        tpl = template_env.get_template("loaddata_predict.py.jinja")
-        code.predict += _render(tpl, dataset=dataset, task=task, script_type="predict")
+        dataset, tmp_code = self._generate_code_load(dataset, task)
+        code += tmp_code
+        dataset, tmp_code = self._generate_code_concat_train_validation(dataset, task)
+        code += tmp_code
+        dataset, tmp_code = self._generate_code_split(dataset, task)
+        code += tmp_code
+        dataset, tmp_code = self._generate_code_subsample(dataset, task)
+        code += tmp_code
+        # set_index msut be before ignore for the case where ignore_columns contain id_columns_for_prediction
+        dataset, tmp_code = self._generate_code_set_index(dataset, task)
+        code += tmp_code
+        dataset, tmp_code = self._generate_code_ignore(dataset, task)
+        code += tmp_code
+        dataset, tmp_code = self._generate_code_set_validation_as_test(dataset, task)
+        code += tmp_code
 
+        return dataset, code
+
+    def _generate_code_load(self, dataset: Dataset, task: Task):
+        code = Code()
+        tpl = template_env.get_template("loaddata.py.jinja")
+        code.validation += _render(tpl, dataset=dataset, task=task, config=self.config, validation=True)
+        code.test += _render(tpl, dataset=dataset, task=task, config=self.config, validation=False)
+        tpl = template_env.get_template("loaddata_train.py.jinja")
+        code.train += _render(tpl, dataset=dataset, task=task, config=self.config)
+        tpl = template_env.get_template("loaddata_predict.py.jinja")
+        code.predict += _render(tpl, dataset=dataset, task=task, config=self.config)
+        return dataset, code
+
+    def _generate_code_set_index(self, dataset: Dataset, task: Task):
+        code = Code()
+        id_columns_for_prediction = self.config.id_columns_for_prediction
+        if id_columns_for_prediction:
+            tpl = template_env.get_template("set_index.py.jinja")
+            code.test += _render(tpl, id_columns_for_prediction=id_columns_for_prediction)
+            code.predict += _render(tpl, id_columns_for_prediction=id_columns_for_prediction)
+        return dataset, code
+
+    def _generate_code_ignore(self, dataset: Dataset, task: Task):
+        code = Code()
+        ignore_columns = dataset.ignore_columns
+        if ignore_columns:
+            tpl = template_env.get_template("drop_ignore_columns.py.jinja")
+            code.validation += _render(tpl, ignore_columns=ignore_columns, train=True, validation=True, test=False)
+            code.test += _render(tpl, ignore_columns=ignore_columns, train=True, validation=False, test=True)
+            code.train += _render(tpl, ignore_columns=ignore_columns, train=True, validation=False, test=False)
+            code.predict += _render(tpl, ignore_columns=ignore_columns, train=False, validation=False, test=True)
+            dataset.training_dataframe = dataset.training_dataframe.drop(
+                dataset.ignore_columns, axis=1, errors="ignore"
+            )
+            if dataset.validation_dataframe is not None:
+                dataset.validation_dataframe = dataset.validation_dataframe.drop(
+                    dataset.ignore_columns, axis=1, errors="ignore"
+                )
+            if dataset.test_dataframe is not None:
+                dataset.test_dataframe = dataset.test_dataframe.drop(dataset.ignore_columns, axis=1, errors="ignore")
+        return dataset, code
+
+    def _generate_code_concat_train_validation(self, dataset: Dataset, task: Task):
+        code = Code()
+        if dataset.validation_dataframe is not None:
+            tpl = template_env.get_template("concat_train_validation.py.jinja")
+            code.test += _render(tpl)
+        return dataset, code
+
+    def _generate_code_split(self, dataset: Dataset, task: Task):
+        code = Code()
         tpl = template_env.get_template("split.py.jinja")
         code.validation += _render(tpl, dataset=dataset, task=task, validation=True)
         code.test += _render(tpl, dataset=dataset, task=task, validation=False)
+        return dataset, code
 
+    def _generate_code_subsample(self, dataset: Dataset, task: Task):
+        code = Code()
         tpl = template_env.get_template("subsample.py.jinja")
         code.validation += _render(tpl, task=task, sample_size=ROW_THRESHOLD_FOR_SAMPLING)
+        return dataset, code
 
-        dataset.training_dataframe = dataset.training_dataframe.drop(dataset.ignore_columns, axis=1, errors="ignore")
-        if dataset.validation_dataframe is not None:
-            dataset.validation_dataframe = dataset.validation_dataframe.drop(
-                dataset.ignore_columns, axis=1, errors="ignore"
-            )
-        if dataset.test_dataframe is not None:
-            dataset.test_dataframe = dataset.test_dataframe.drop(dataset.ignore_columns, axis=1, errors="ignore")
-
+    def _generate_code_set_validation_as_test(self, dataset: Dataset, task: Task):
+        code = Code()
+        tpl = template_env.get_template("set_validation_as_test.py.jinja")
+        code.validation += _render(tpl)
         return dataset, code
